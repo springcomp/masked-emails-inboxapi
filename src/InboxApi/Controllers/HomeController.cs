b6fc7e69743856eb556cd1451f81adefce638e7b
Maxime Labelle <springcomp@users.noreply.github.com>
2020-01-31 19:50:17 +0100

    Return valid HTML page on GET / HTTP/1.1

---
+++ src/InboxApi/Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace InboxApi.Controllers
+{
+    [ApiController()]
+    [Route("")]
+    public sealed class HomeController : ControllerBase
+    {
+        [HttpGet()]
+        [Route("")]
+        public ContentResult Index()
+        {
+            return Content(@"
+<html>
+<head>
+<link href=""https://fonts.googleapis.com/css?family=Roboto:300,400,500&display=swap"" rel=""stylesheet"">
+<link href=""https://fonts.googleapis.com/icon?family=Material+Icons"" rel=""stylesheet"">
+</head>
+<body>
+  <span class=""title"">Masked Emails Inbox API</span>
+</body></html>
+", "text/html");
+        }
+    }
+}
